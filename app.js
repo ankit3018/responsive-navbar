e3539afc44ecb63700814de7fbec6daf11a8582e
ankit3018 <84768811+ankit3018@users.noreply.github.com>
2021-10-04 15:31:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const toggleView=document.querySelector('.bx-menu');
+// const linkView=document.querySelector('.links');
+const navbarView=document.querySelector('.navbar');
+toggleView.addEventListener('click',()=>{
+    navbarView.classList.toggle('functionality-js')
+})
+
